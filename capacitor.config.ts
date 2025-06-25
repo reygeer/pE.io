05f6949cd2ec7d2ea8063bf0fa09f7c65f8f7003
reygeer <delarajuan2703@gmail.com>
2025-06-25 02:42:03 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Almacen_Proyect',
+  webDir: 'www'
+};
+
+export default config;
